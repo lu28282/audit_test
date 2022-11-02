51f0373232bf7993bf67656d22620f88b27aee29
Luca Schmitz <schmitz@sipgate.de>
2022-11-02 13:58:37 +0100

    use configuration Bean

--- src/main/java/com/example/demo/config/DebeziumConnectorConfig.java
+++ src/main/java/com/example/demo/config/DebeziumConnectorConfig.java
@@ -28,57 +28,42 @@ public class DebeziumConnectorConfig {
     @Value("${customer.datasource.password}")
     private String customerDbPassword;
 
-    // /**
-    //  * Customer Database Connector Configuration
-    //  * Adapt connector class to fit source database.
-    //  * Adapt offsetStorage to change data processing
-    //  */
-    // @Bean
-    // public io.debezium.config.Configuration customerConnector() throws IOException {
-    //     File offsetStorageTempFile = File.createTempFile("offsets_", ".dat");
-    //     File dbHistoryTempFile = File.createTempFile("dbhistory_", ".dat");
-    //     System.out.println("==========================================================================================");
-    //     System.out.println(customerDbHost);
-    //     System.out.println(customerDbPort);
-    //     System.out.println(customerDbUsername);
-    //     System.out.println(customerDbPassword);
-    //     System.out.println(customerDbName);
-    //     System.out.println(offsetStorageTempFile.getAbsolutePath());
-    //     System.out.println(dbHistoryTempFile.getAbsolutePath());
-    //     System.out.println("==========================================================================================");
+    /**
+     * Customer Database Connector Configuration
+     * Adapt connector class to fit source database.
+     * Adapt offsetStorage to change data processing
+     */
+    @Bean
+    public io.debezium.config.Configuration customerConnector() throws IOException {
+        File offsetStorageTempFile = File.createTempFile("offsets_", ".dat");
+        File dbHistoryTempFile = File.createTempFile("dbhistory_", ".dat");
+        System.out.println("==========================================================================================");
+        System.out.println(customerDbHost);
+        System.out.println(customerDbPort);
+        System.out.println(customerDbUsername);
+        System.out.println(customerDbPassword);
+        System.out.println(customerDbName);
+        System.out.println(offsetStorageTempFile.getAbsolutePath());
+        System.out.println(dbHistoryTempFile.getAbsolutePath());
+        System.out.println("==========================================================================================");
         
-    //     return io.debezium.config.Configuration.create()
-    //             .with("name", "engine")
-    //             .with("connector.class", "io.debezium.connector.mysql.MySqlConnector")
-    //             .with("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore")
-    //             .with("offset.storage.file.filename", offsetStorageTempFile.getAbsolutePath())
-    //             .with("database.hostname", customerDbHost)
-    //             .with("database.port", customerDbPort)
-    //             .with("database.user", customerDbUsername)
-    //             .with("database.password", customerDbPassword)
-    //             .with("database.server.id", "1")
-    //             .with("schema.history.internal",
-    //             "io.debezium.storage.file.history.FileSchemaHistory")
-    //             .with("database.history.internal.file.filename", dbHistoryTempFile.getAbsolutePath())
-    //             .build();
-    //     // return io.debezium.config.Configuration.create()
-    //     //         .with("name", "customer-mysql-connector")
-    //     //         .with("connector.class", "io.debezium.connector.mysql.MySqlConnector")
-    //     //         .with("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore")
-    //     //         .with("offset.storage.file.filename", offsetStorageTempFile.getAbsolutePath())
-    //     //         .with("offset.flush.interval.ms", "60000")
-    //     //         .with("database.hostname", customerDbHost)
-    //     //         .with("database.port", customerDbPort)
-    //     //         .with("database.user", customerDbUsername)
-    //     //         .with("database.password", customerDbPassword)
-    //     //         .with("database.dbname", customerDbName)
-    //     //         .with("database.include.list", customerDbName)
-    //     //         .with("include.schema.changes", "false")
-    //     //         .with("database.allowPublicKeyRetrieval", "true")
-    //     //         .with("database.server.id", "1")
-    //     //         .with("database.server.name", "customerdb")
-    //     //         .with("database.history", "io.debezium.relational.history.FileDatabaseHistory")
-    //     //         .with("database.history.file.filename", dbHistoryTempFile.getAbsolutePath())
-    //     //         .build();
-    // }
+        return io.debezium.config.Configuration.create()
+                .with("name", "engine")
+                .with("connector.class", "io.debezium.connector.mysql.MySqlConnector")
+                .with("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore")
+                .with("offset.storage.file.filename", offsetStorageTempFile.getAbsolutePath())
+                .with("offset.flush.interval.ms", "60000")
+                .with("database.hostname", customerDbHost)
+                .with("database.port", customerDbPort)
+                .with("database.user", customerDbUsername)
+                .with("database.password", customerDbPassword)
+                .with("include.schema.changes", "false")
+                .with("database.server.id", "1")
+                .with("topic.prefix", "my-app-connector")
+                .with("database.allowPublicKeyRetrieval", "true")
+                .with("schema.history.internal",
+                "io.debezium.storage.file.history.FileSchemaHistory")
+                .with("schema.history.internal.file.filename", dbHistoryTempFile.getAbsolutePath())
+                .build();
+    }
 }
--- src/main/java/com/example/demo/listener/DebeziumListener.java
+++ src/main/java/com/example/demo/listener/DebeziumListener.java
@@ -1,5 +1,6 @@
 package com.example.demo.listener;
 
+import io.debezium.config.Configuration;
 import io.debezium.embedded.Connect;
 import io.debezium.engine.DebeziumEngine;
 import io.debezium.engine.RecordChangeEvent;
@@ -7,64 +8,26 @@ import io.debezium.engine.format.ChangeEventFormat;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.source.SourceRecord;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Properties;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-
-import static io.debezium.data.Envelope.FieldName.*;
-import static io.debezium.data.Envelope.Operation;
-
-// import com.example.demo.service.CustomerService;
 
 @Slf4j
 @Component
 public class DebeziumListener {
 
     private final Executor executor = Executors.newSingleThreadExecutor();
-    // private final CustomerService customerService;
     private final DebeziumEngine<RecordChangeEvent<SourceRecord>> debeziumEngine;
 
-    // public DebeziumListener(Configuration customerConnectorConfiguration, CustomerService customerService) throws IOException {
-    public DebeziumListener() throws IOException {
-
-        // ============================
-        // Configuration for mysql connector
-        File offsetStorageTempFile = File.createTempFile("offsets_", ".dat");
-        File dbHistoryTempFile = File.createTempFile("dbhistory_", ".dat");
-        final Properties props = new Properties();
-        props.setProperty("name", "engine");
-        props.setProperty("connector.class", "io.debezium.connector.mysql.MySqlConnector");
-        props.setProperty("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore");
-        props.setProperty("offset.storage.file.filename", offsetStorageTempFile.getAbsolutePath());
-        props.setProperty("offset.flush.interval.ms", "60000");
-        /* begin connector properties */
-        props.setProperty("database.hostname", "localhost");
-        props.setProperty("database.hostname", "localhost");
-        props.setProperty("database.port", "3306");
-        props.setProperty("database.user", "user");
-        props.setProperty("database.password", "password");
-        props.setProperty("include.schema.changes", "false");
-        props.setProperty("database.server.id", "1");
-        props.setProperty("topic.prefix", "my-app-connector");
-        props.setProperty("database.allowPublicKeyRetrieval", "true");
-        props.setProperty("schema.history.internal",
-                "io.debezium.storage.file.history.FileSchemaHistory");
-        props.setProperty("schema.history.internal.file.filename", dbHistoryTempFile.getAbsolutePath());
-        // ============================
+    public DebeziumListener(Configuration customerConnectorConfiguration) throws IOException {
 
         this.debeziumEngine = DebeziumEngine.create(ChangeEventFormat.of(Connect.class))
-                .using(props)
+                .using(customerConnectorConfiguration.asProperties())
                 .notifying(this::handleChangeEvent)
                 .build();
-
-
-        // this.customerService = customerService;
     }
 
     private void handleChangeEvent(RecordChangeEvent<SourceRecord> sourceRecordRecordChangeEvent) {
@@ -73,26 +36,6 @@ public class DebeziumListener {
         log.info("Key = '" + sourceRecord.key() + "' value = '" + sourceRecord.value() + "'");
         System.out.println("Key = '" + sourceRecord.key() + "' value = '" + sourceRecord.value() + "'");
 
-        Struct sourceRecordChangeValue = (Struct) sourceRecord.value();
-
-        if (sourceRecordChangeValue != null) {
-            Operation operation = Operation.forCode((String) sourceRecordChangeValue.get(OPERATION));
-
-            // if(operation != Operation.READ) {
-            // String record = operation == Operation.DELETE ? BEFORE : AFTER; // Handling
-            // Update & Insert operations.
-
-            // Struct struct = (Struct) sourceRecordChangeValue.get(record);
-            // Map<String, Object> payload = struct.schema().fields().stream()
-            // .map(Field::name)
-            // .filter(fieldName -> struct.get(fieldName) != null)
-            // .map(fieldName -> Pair.of(fieldName, struct.get(fieldName)))
-            // .collect(toMap(Pair::getKey, Pair::getValue));
-
-            // this.customerService.replicateData(payload, operation);
-            // log.info("Updated Data: {} with Operation: {}", payload, operation.name());
-            // }
-        }
     }
 
     @PostConstruct
